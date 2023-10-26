a26812517ecf89f063ad7955d5195f9bd5d9f9c7
AnkurBansal18 <ankurbansal2017@gmail.com>
2023-10-26 20:18:14 +0530

    added env variables

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,5 @@ COPY . .
 # ci - clean install (recommended)
 RUN npm ci
 
+ENV PORT 3000
 CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
 app.get("/", (req, res) => {
   res.json({
     message: "hello moto",
   });
 });
-app.listen(3001, () => {
-  console.log("server started on port 3001 🚀");
+app.listen(process.env.PORT, () => {
+  console.log(`server started on port ${process.env.PORT} 🚀`);
 });
